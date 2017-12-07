5364156fecf8cf185707bba3144467bb5ea0bf78
Vastquery <vastqueryproject@gmail.com>
2017-12-07 12:10:04 +0530

    second changes

--- app/src/main/java/com/example/www/toppage/MainActivity.java
+++ app/src/main/java/com/example/www/toppage/MainActivity.java
@@ -41,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
         final ActionBar ab = getSupportActionBar();
         ab.setHomeAsUpIndicator(R.drawable.ic_menu);
         ab.setDisplayHomeAsUpEnabled(true);
